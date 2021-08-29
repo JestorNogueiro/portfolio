b9e58dbcd5ac8b3bdf48ec9226d61686cad14c90
JestorNogueiro <jestornogueiro789@gmail.com>
2021-08-30 00:08:42 +0530

    added tailwind css and component

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+  <div className='grid grid-cols-12 gap-6 px-5 lg:px-25 my-14 sm:px-20 md:px-32'>
+<div className=' lg:col-span-3 bg-white rounded-2xl col-span-12 px-3 text-center'>
+  <Sidebar/>
+</div>
+<div className=' lg:col-span-9 grid-cols-9 bg-white rounded-2xl col-span-12 text-center'> 
+<Navbar/>
+ <Component {...pageProps} />
+</div>
+  </div> 
+  )}
+
+export default MyApp
