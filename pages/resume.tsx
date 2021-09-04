016543edb2b4048778cd2b882a5651640a2e4865
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-04 23:00:59 +0530

    added education details

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,9 +1,27 @@
 function resume() {
- return (
-  <div>
-   resume 
-  </div>
- )
+  return (
+    <div>
+      <div className="grid md:grid-cols-2">
+        <div>
+          <h2 className="text-2xl font-bold my-3">Education</h2>
+          <div className="text-left px-3">
+            <h2 className="font-bold text-xl my-2">
+              Master Of Computer Applicaton
+            </h2>
+            <p className="font-medium">Year 2018-20</p>
+            <p>
+              Did MCA in Jain college of engineering, Balagavi Karnataka India
+            </p>
+          </div>
+        </div>
+        <div>
+          <h2>Experince</h2>
+          <p>Internship</p>
+          <p>Did six months Internship in technomind It Solutions</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default resume
+export default resume;
