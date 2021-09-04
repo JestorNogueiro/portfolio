320c0585e0402aaf1bb72966125f3425dfc589b3
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-05 00:03:40 +0530

    Added experience section

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -2,10 +2,10 @@ function resume() {
   return (
     <div>
       <div className="grid md:grid-cols-2">
-        <div>
-          <h2 className="text-2xl font-bold my-3">Education</h2>
+        <div className="mx-2 border-r">
+          <h2 className="text-2xl font-bold my-3 ml-4">Education</h2>
           <div className="text-left px-3">
-            <h2 className="font-bold text-xl my-2">
+            <h2 className="font-bold text-xl my-2 border-b-2">
               Master Of Computer Applicaton
             </h2>
             <p className="font-medium">Year 2018-20</p>
@@ -14,10 +14,52 @@ function resume() {
             </p>
           </div>
         </div>
+        <div className="mx-2">
+          <h2 className="text-2xl font-bold my-3 ml-4">Experince</h2>
+          <p className="font-bold text-xl my-2 ">
+            <a
+              href="https://www.tmits.in/"
+              target="_blank"
+              rel="technomind"
+              className="border-b-2 "
+            >
+              <i className="text-blue-900 font-semibold">
+                {" "}
+                Technominds IT Solutions
+              </i>{" "}
+            </a>
+          </p>
+          <p className="mb-1">
+            10<small>th</small> Jan 2020 - 10<small>th</small> july 2020
+          </p>
+          <p>
+            worked as Front-end Developer and PHP Developer for 6 months with
+            the project.
+          </p>
+          <p>
+            Worked in
+            <a
+              href="https://www.tmits.in/"
+              target="_blank"
+              rel="technomind"
+              className="border-b-2 "
+            >
+              <i className="text-blue-900 font-semibold">
+                {" "}
+                Technominds IT Solutions,
+              </i>{" "}
+            </a>
+            Belgaum karnataka.
+          </p>
+        </div>
+      </div>
+      <div className="grid gap-6 md:grid-cols-2">
         <div>
-          <h2>Experince</h2>
-          <p>Internship</p>
-          <p>Did six months Internship in technomind It Solutions</p>
+          <h1>Languages & Framwork</h1>
+          <div>{/* Images */}</div>
+        </div>
+        <div>
+          <h2>Tools</h2>
         </div>
       </div>
     </div>
