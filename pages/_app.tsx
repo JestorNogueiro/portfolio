4a8acb84cc2b0602a3949a39cb36f0bbb7b9dd69
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-06 00:42:54 +0530

    added dark mode theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,20 @@
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import "../styles/globals.css";
-
+import { ThemeProvider } from "next-themes";
 function MyApp({ Component, pageProps }) {
   return (
-    <div className=" grid grid-cols-12 gap-6 px-5 lg:px-25 my-14 sm:px-20 md:px-32 space-x-4">
-      <div className=" lg:col-span-3 bg-white rounded-2xl col-span-12 px-3 text-center md:min-w-min">
-        <Sidebar />
+    <ThemeProvider attribute="class">
+      <div className="grid grid-cols-12 gap-6 px-5 lg:px-25 my-14 sm:px-20 md:px-32 space-x-4">
+        <div className=" lg:col-span-3 bg-white rounded-2xl col-span-12 px-3 text-center md:min-w-min dark:bg-darkcolor">
+          <Sidebar />
+        </div>
+        <div className=" lg:col-span-9 grid-cols-9 bg-white rounded-2xl col-span-12 dark:bg-darkcolor">
+          <Navbar />
+          <Component {...pageProps} />
+        </div>
       </div>
-      <div className=" lg:col-span-9 grid-cols-9 bg-white rounded-2xl col-span-12">
-        <Navbar />
-        <Component {...pageProps} />
-      </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
