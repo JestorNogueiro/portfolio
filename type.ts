884c2e10b2b2aae69cee7b0abbd65d4879e54835
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-06 00:42:16 +0530

    added interface for project

--- type.ts
+++ type.ts
@@ -4,3 +4,15 @@ export interface IService {
   about: string;
   Icon: IconType;
 }
+
+export interface Project {
+  name: string;
+  description: string;
+  image_path: string;
+  deployed_url: string;
+  github_url: string;
+  category: Category[];
+  tech: string[];
+}
+
+export type Category = "react" | "next" | "native";
