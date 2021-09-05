652150239934bdbdd2d6d6f40be29b6869a144dd
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-06 00:53:26 +0530

    rendering name

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -4,9 +4,17 @@ import { Project } from "../type";
 const ProjectCard: FunctionComponent<{
   projectData: Project;
 }> = ({
-  projectData: { name, description, deployed_url, github_url, category, tech },
+  projectData: {
+    name,
+    description,
+    deployed_url,
+    github_url,
+    category,
+    tech,
+    image_path,
+  },
 }) => {
-  return <div></div>;
+  return <div>{name}</div>;
 };
 
 export default ProjectCard;
