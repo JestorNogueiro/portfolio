35d0add56e990742dc8557bfa48c8391c45bec4d
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-06 15:24:10 +0530

    added details of each project

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,6 +1,8 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
+import { FaGithub } from "react-icons/fa";
 import { Project } from "../type";
-
+import Image from "next/image";
+import { IoIosClose } from "react-icons/io";
 const ProjectCard: FunctionComponent<{
   projectData: Project;
 }> = ({
@@ -14,7 +16,45 @@ const ProjectCard: FunctionComponent<{
     image_path,
   },
 }) => {
-  return <div>{name}</div>;
+  const [showDetail, setShowDetail] = useState(false);
+  return (
+    <div className="">
+      <Image
+        src={image_path}
+        alt={name}
+        height={180}
+        width={250}
+        onClick={() => setShowDetail(true)}
+        className="cursor-pointer transform hover:scale-95 duration-500"
+      />
+      <p>{name}</p>
+      {showDetail && (
+        <div className="flex absolute top-0 left-0 bg-white dark:bg-darkback h-auto w-full z-10  p-4 shadow-2xl border-2 rounded-xl dark:border-gray-600">
+          <div className="grid md:grid-cols-2">
+            <div>
+              <Image src={image_path} alt={name} height={180} width={300} />
+              <p className="flex mt-4 gap-3 items-center  ">
+                <FaGithub className="h-8 w-8" />
+                <p> Demo</p>
+              </p>
+            </div>
+            <div>
+              <h2>{name}</h2>
+              <p>{description}</p>
+              <p className="space-x-4">
+                {tech.map((item) => (
+                  <span key={item}>{item}</span>
+                ))}
+              </p>
+            </div>
+            <button onClick={() => setShowDetail(false)}>
+              <IoIosClose className="h-8 w-8 absolute top-0 right-0 bg-gray-600 text-white rounded-full dark:bg-gray-400m m-3 transform hover:rotate-90 duration-500 " />
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ProjectCard;
