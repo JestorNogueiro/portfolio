56637717171e07410c49fb8cca93f2e8e730fee7
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-07 23:32:28 +0530

    added some styles and view port heigth

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,13 +3,16 @@ import { project } from "../data";
 
 function projects() {
   return (
-    <div className="my-2 p-2">
+    <div
+      className="my-2 py-2 px-5 overflow-y-scroll"
+      style={{ height: "85vh" }}
+    >
       <nav>Navbar</nav>
       <div className="relative grid grid-cols-12 gap-4 my-3 ">
         {project.map((project) => (
           <div
             key={project.name}
-            className=" col-span-12 p-2 sm:col-span-6 lg:col-span-4"
+            className=" col-span-12 p-2 sm:col-span-6 lg:col-span-4  dark:bg-gray-900 bg-gray-100 justify-center"
           >
             <ProjectCard projectData={project} />
           </div>
