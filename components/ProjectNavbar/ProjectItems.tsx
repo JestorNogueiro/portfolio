1ccb2346fee68ccf8d740670cf9329e8ac2a9cce
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-07 23:54:49 +0530

    added styles

--- components/ProjectNavbar/ProjectItems.tsx
+++ components/ProjectNavbar/ProjectItems.tsx
@@ -4,7 +4,9 @@ import { Category } from "../../type";
 const ProjectItems: FunctionComponent<{ value: Category | "all" }> = ({
   value,
 }) => {
-  return <div>{value}</div>;
+  return (
+    <div className="cursor-pointer hover:text-green capitalize">{value}</div>
+  );
 };
 
 export default ProjectItems;
