5e6d680864f794205bb78cc631f4eaa90b2bc929
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-07 23:51:46 +0530

    imported project item and asigned value

--- components/ProjectNavbar/index.tsx
+++ components/ProjectNavbar/index.tsx
@@ -1,5 +1,15 @@
+import ProjectItems from "./ProjectItems";
+
 const ProjectNavbar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <ProjectItems value="all" />
+      <ProjectItems value="react" />
+      <ProjectItems value="next" />
+      <ProjectItems value="native" />
+      {/* <ProjectItems value="others"/> */}
+    </div>
+  );
 };
 
 export default ProjectNavbar;
