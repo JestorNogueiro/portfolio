c454d9b05c76d03ae72df660f624505b51d9df40
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-08 17:06:59 +0530

    added projectItem component for filtering

--- components/ProjectNavbar/index.tsx
+++ components/ProjectNavbar/index.tsx
@@ -1,12 +1,31 @@
-import ProjectItems from "./ProjectItems";
+import { FunctionComponent } from "react";
+// import ProjectItems from "./ProjectItems";
+import { Category } from "../../type";
 
-const ProjectNavbar = () => {
+const ProjectItems: FunctionComponent<{
+  value: Category | "all";
+  filterData: Function;
+}> = ({ value, filterData }) => {
+  const filterCat = () => filterData();
+  return (
+    <li
+      onClick={filterCat}
+      className="cursor-pointer hover:text-green capitalize list-none"
+    >
+      {value}
+    </li>
+  );
+};
+
+const ProjectNavbar: FunctionComponent<{ filterData: Function }> = ({
+  filterData,
+}) => {
   return (
     <div className="flex px-3 py-2 space-x-3 overflow-x-auto ">
-      <ProjectItems value="all" />
-      <ProjectItems value="react" />
-      <ProjectItems value="next" />
-      <ProjectItems value="native" />
+      <ProjectItems value="all" filterData={filterData} />
+      <ProjectItems value="react" filterData={filterData} />
+      <ProjectItems value="next" filterData={filterData} />
+      <ProjectItems value="native" filterData={filterData} />
       {/* <ProjectItems value="others"/> */}
     </div>
   );
