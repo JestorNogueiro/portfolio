18f9727536f7ba07925ec9011354a640e5fa64c7
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-08 17:01:02 +0530

    changed name

--- data.ts
+++ data.ts
@@ -15,6 +15,12 @@ export const servicesProvided: IService[] = [
       "I can build Native app ( both Android and IOS ) using <b>React Native</b>",
     Icon: RiComputerLine,
   },
+  {
+    title: "Programmer",
+    about:
+      "I can build Native app ( both Android and IOS ) using <b>React Native</b>",
+    Icon: RiComputerLine,
+  },
 ];
 
 export const languagesKnown = [
@@ -31,7 +37,7 @@ export const project: Project[] = [
     deployed_url: "https://disney-clone-ashy.vercel.app/",
     github_url: "https://github.com/JestorNogueiro/disney-clone",
     category: ["next"],
-    tech: ["reactjs", "nextjs", "hero Icons", "Tailwind css", "react-player"],
+    tech: ["react.js", "next.js", "hero Icons", "Tailwind css", "react-player"],
   },
   {
     name: "Airbnb ",
@@ -40,7 +46,7 @@ export const project: Project[] = [
     deployed_url: "https://airbnb-jestornogueiro.vercel.app/",
     github_url: "https://github.com/JestorNogueiro/airbnb",
     category: ["next"],
-    tech: ["reactjs", "nextjs", "tailwindcss", "hero Icons", "react-map"],
+    tech: ["react.js", "next.js", "tailwindCss", "hero Icons", "react-map"],
   },
   {
     name: "Tesla",
@@ -49,7 +55,7 @@ export const project: Project[] = [
     deployed_url: "https://tesla-clone-flax.vercel.app/",
     github_url: "https://github.com/JestorNogueiro/Tesla-clone",
     category: ["react"],
-    tech: ["reactjs", "material UI", "styled-components", "firebase"],
+    tech: ["react.js", "material UI", "styled-components", "firebase"],
   },
   {
     name: "Netflix",
@@ -58,7 +64,7 @@ export const project: Project[] = [
     deployed_url: "https://netflix-clone-129c2.web.app/",
     github_url: "https://github.com/JestorNogueiro/Netflix-Clone",
     category: ["react"],
-    tech: ["reactjs", "material UI", "axios", "firebase"],
+    tech: ["react.js", "material UI", "axios", "firebase"],
   },
   {
     name: "Covid Tracker",
@@ -68,20 +74,20 @@ export const project: Project[] = [
     github_url: "https://github.com/JestorNogueiro/covid19-tracker",
     category: ["react"],
     tech: [
-      "reactjs",
+      "react.js",
       "material UI",
       "react-leaflet (map)",
-      "chartjs",
+      "chart.js",
       "firebase",
     ],
   },
   {
-    name: "Covid Tracker",
+    name: "Amazon clone",
     description: "this is clone made with next js",
     image_path: "/scene.jpg",
     deployed_url: "",
     github_url: "https://github.com/JestorNogueiro/amazon-clone",
     category: ["react"],
-    tech: ["reactjs", "material UI", "firebase"],
+    tech: ["react.js", "material UI", "firebase"],
   },
 ];
