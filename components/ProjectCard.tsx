21156d7b6fc4a9ae6dd16551520ccfbba44d340b
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-09 16:46:17 +0530

    added animation to image section and details section

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -4,6 +4,8 @@ import { Project } from "../type";
 import Image from "next/image";
 import { IoIosClose } from "react-icons/io";
 import { AiFillProject } from "react-icons/ai";
+import { motion } from "framer-motion";
+import { fadeInUp, stagger } from "../animation";
 const ProjectCard: FunctionComponent<{
   projectData: Project;
 }> = ({
@@ -38,16 +40,28 @@ const ProjectCard: FunctionComponent<{
       {/* when it is true the project details will appear */}
       {showDetail && (
         <div className="flex md:absolute sticky top-0 left-0 bg-white dark:bg-darkback h-auto w-full z-20  p-4 shadow-2xl border-2 rounded-xl dark:border-gray-600">
-          <div className="grid md:grid-cols-2 grid-cols-1 ">
-            <div className="flex flex-col items-center justify-center py-3">
+          <motion.div
+            variants={stagger}
+            initial="initial"
+            animate="animate"
+            className="grid md:grid-cols-2 grid-cols-1 "
+          >
+            <motion.div
+              variants={fadeInUp}
+              className="flex flex-col items-center justify-center py-3"
+            >
               <Image
                 src={image_path}
                 alt={name}
                 height={180}
                 width={300}
+                className="bg-gray-800"
                 // layout="responsive"
               />
-              <div className="flex mt-4 gap-3 justify-center ">
+              <motion.div
+                variants={fadeInUp}
+                className="flex mt-4 gap-3 justify-center "
+              >
                 <a
                   href={github_url}
                   target="_blank"
@@ -61,19 +75,31 @@ const ProjectCard: FunctionComponent<{
                   href={deployed_url}
                   target="_blank"
                   rel="noreferrer"
-                  className="bg-gray-200 dark:bg-gray-900 mr-2   h-8
+                  className="bg-gray-200 dark:bg-gray-900 ml-2   h-8
                   min-w-max flex items-center py-2 px-3"
                 >
                   <AiFillProject />
                   <span>Demo</span>
                 </a>
-              </div>
-            </div>
+              </motion.div>
+            </motion.div>
             {/* project details */}
-            <div className="ml-4 ">
-              <h2 className="mb-3 text-xl font-medium md:text-2xl">{name}</h2>
-              <p className="mb-3 font-medium">{description}</p>
-              <div className=" flex flex-wrap items-center justify-center mt-5 space-x-2 tracking-wider ">
+            <motion.div
+              variants={stagger}
+              initial="initial"
+              animate="animate"
+              className="mx-4 mt-3 "
+            >
+              <h2 className="mb-3 text-xl font-semibold md:text-2xl border-b border-gray-800 ">
+                {name}
+              </h2>
+              <motion.p variants={fadeInUp} className="mb-3 font-medium">
+                {description}
+              </motion.p>
+              <motion.div
+                variants={fadeInUp}
+                className=" flex flex-wrap mt-5 space-x-2 tracking-wider "
+              >
                 {tech.map((item) => (
                   <span
                     className="bg-gray-200 dark:bg-gray-900 px-3 py-1 my-4"
@@ -82,13 +108,13 @@ const ProjectCard: FunctionComponent<{
                     {item}
                   </span>
                 ))}
-              </div>
-            </div>
+              </motion.div>
+            </motion.div>
             {/* button to close the project details */}
             <button onClick={() => setShowDetail(false)}>
               <IoIosClose className="h-8 w-8 absolute top-0 right-0 bg-gray-600 text-white rounded-full dark:bg-gray-700 m-3 transform hover:rotate-90 duration-500 " />
             </button>
-          </div>
+          </motion.div>
         </div>
       )}
     </div>
