fab1a633aced4dad5295fd6fc7716ebd2548a20d
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-09 16:44:48 +0530

    added animation presence and key prop

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,8 @@ import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
-function MyApp({ Component, pageProps }) {
+import { AnimatePresence } from "framer-motion";
+function MyApp({ Component, pageProps, router }) {
   return (
     <ThemeProvider attribute="class">
       <div className="grid grid-cols-12 gap-6 px-5 lg:px-25 my-14 sm:px-20 md:px-32 space-x-4">
@@ -11,7 +12,9 @@ function MyApp({ Component, pageProps }) {
         </div>
         <div className=" lg:col-span-9 grid-cols-9 bg-white rounded-2xl col-span-12 dark:bg-darkcolor">
           <Navbar />
-          <Component {...pageProps} />
+          <AnimatePresence>
+            <Component {...pageProps} key={router.route} />
+          </AnimatePresence>
         </div>
       </div>
     </ThemeProvider>
