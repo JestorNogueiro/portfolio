77e955508ecda9105d60dd2f8ca2f36ef03d072e
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-09 13:51:51 +0530

    added fade in up animation

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -9,25 +9,36 @@ import {
 import { DiCss3 } from "react-icons/di";
 import { SiTypescript } from "react-icons/si";
 import LanguageDetails from "../components/LanguageDetails";
-
+import { motion } from "framer-motion";
+import { fadeInUp } from "../animation";
 function resume() {
   return (
-    <div>
+    <div className="px-4 py-2">
       <div className="grid md:grid-cols-2">
-        <div className="mx-2 border-r">
+        <motion.div
+          variants={fadeInUp}
+          initial="initial"
+          animate="animate"
+          className="mx-2 border-r"
+        >
           <h2 className="text-2xl font-bold my-3 ml-4">Education</h2>
           <div className="text-left px-3">
             <h2 className="font-bold text-xl my-2 border-b-2">
-              Master Of Computer Applicaton
+              Master Of Computer Applications
             </h2>
             <p className="font-medium">Year 2018-20</p>
             <p>
               Did MCA in Jain college of engineering, Balagavi Karnataka India
             </p>
           </div>
-        </div>
-        <div className="mx-2">
-          <h2 className="text-2xl font-bold my-3 ml-4">Experince</h2>
+        </motion.div>
+        <motion.div
+          variants={fadeInUp}
+          initial="initial"
+          animate="animate"
+          className="mx-2"
+        >
+          <h2 className="text-2xl font-bold my-3 ml-4">Experience</h2>
           <p className="font-bold text-xl my-2 ">
             <a
               href="https://www.tmits.in/"
@@ -63,12 +74,17 @@ function resume() {
             </a>
             Belgaum karnataka.
           </p>
-        </div>
+        </motion.div>
       </div>
       <div className="grid gap-6 md:grid-cols-2 my-6 mx-4">
-        <div className="border-r pr-4">
-          <h1 className="text-2xl font-bold my-3">Languages & Framwork</h1>
-          <div className="flex flex-wrap space-x-3">
+        <div className="border-r mr-4">
+          <h1 className="text-2xl font-bold my-3">Languages & Framework</h1>
+          <motion.div
+            variants={fadeInUp}
+            initial="initial"
+            animate="animate"
+            className="flex flex-wrap space-x-3 p-2"
+          >
             {/* Images */}
             {/* react */}
             <LanguageDetails title="HTML" Icon={FaHtml5} />
@@ -77,9 +93,9 @@ function resume() {
             <LanguageDetails title="reactjs" Icon={FaReact} />
             <LanguageDetails title="nextjs" Icon={SiNextDotJs} />
             <LanguageDetails title="reactNative" Icon={FaReact} />
-          </div>
+          </motion.div>
         </div>
-        <div>
+        <motion.div variants={fadeInUp} initial="initial" animate="animate">
           <h2 className="text-2xl font-bold my-3">Other Tools & Tech</h2>
           <div className=" flex flex-wrap">
             <LanguageDetails title="jquery" Icon={SiJquery} />
@@ -93,7 +109,7 @@ function resume() {
               Icon={SiStyledComponents}
             />
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
