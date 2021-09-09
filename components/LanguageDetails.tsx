4ca3b9556a4cf48bd96bdd51a8df775a17bd9bc1
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-09 16:45:13 +0530

    small change

--- components/LanguageDetails.tsx
+++ components/LanguageDetails.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { fadeInUp, stagger } from "../animation";
+import { fadeInUp } from "../animation";
 function LanguageDetails({ title, Icon }) {
   return (
     <motion.div
