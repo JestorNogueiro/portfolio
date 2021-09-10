ffe87cacbf8dc3fa3c28024c401841a28e7db961
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-10 23:13:01 +0530

    adjusted margin left of sidebar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,11 @@ import { AnimatePresence } from "framer-motion";
 function MyApp({ Component, pageProps, router }) {
   return (
     <ThemeProvider attribute="class">
-      <div className="grid grid-cols-12 gap-6 px-5 lg:px-25 my-14 sm:px-20 md:px-32 space-x-4">
-        <div className=" lg:col-span-3 bg-white rounded-2xl col-span-12 px-3 text-center md:min-w-min dark:bg-darkcolor">
+      <div className="grid grid-cols-12 gap-6 px-5 lg:px-25 my-14 sm:px-20 md:px-32 space-x-4 ">
+        <div className=" lg:col-span-3 bg-white rounded-2xl col-span-12 px-3 text-center md:min-w-min dark:bg-darkcolor ml-3 ">
           <Sidebar />
         </div>
-        <div className=" flex flex-col overflow-hidden lg:col-span-9 grid-cols-9 bg-white rounded-2xl col-span-12 dark:bg-darkcolor shadow-2xl -ml-4">
+        <div className=" flex flex-col overflow-hidden lg:col-span-9 grid-cols-9 bg-white rounded-2xl col-span-12 dark:bg-darkcolor shadow-2xl ">
           <Navbar />
           <AnimatePresence exitBeforeEnter>
             <Component {...pageProps} key={router.route} />
