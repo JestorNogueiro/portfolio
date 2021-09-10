6bd857de8e75ae40b02c102a2f7a87a4a093da16
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-10 18:22:01 +0530

    Updated project details

--- data.ts
+++ data.ts
@@ -1,4 +1,3 @@
-import next from "next";
 import { RiComputerLine } from "react-icons/ri";
 import { IService, Project } from "./type";
 
@@ -16,24 +15,20 @@ export const servicesProvided: IService[] = [
     Icon: RiComputerLine,
   },
   {
-    title: "Programmer",
+    title: "Quick Learner",
     about:
-      "I can build Native app ( both Android and IOS ) using <b>React Native</b>",
+      "I am a quick learner, I can learn and adapt new <b> Technology </b>and <b>framework</b> easily ",
     Icon: RiComputerLine,
   },
 ];
 
-export const languagesKnown = [
-  {
-    title: "react",
-  },
-];
-
+// Project Details
 export const project: Project[] = [
   {
     name: "Disney+ clone",
-    description: "this is clone made with next js",
-    image_path: "/scene.jpg",
+    description:
+      "In this you can view different movies and the Tv series, you can view there details and also view trailer of the particular movie and see no of  seasons available  ",
+    image_path: "/project/disney.jpg",
     deployed_url: "https://disney-clone-ashy.vercel.app/",
     github_url: "https://github.com/JestorNogueiro/disney-clone",
     category: ["next"],
@@ -41,8 +36,9 @@ export const project: Project[] = [
   },
   {
     name: "Airbnb ",
-    description: "this is airbnb clone made with next js",
-    image_path: "/scene.jpg",
+    description:
+      "It is static website, you can search any country and select the date and other details and search the place, also map is implemented (map only can be viewed on desktop) Note:Only London data is implemented statically   ",
+    image_path: "/project/airbnb.jpg",
     deployed_url: "https://airbnb-jestornogueiro.vercel.app/",
     github_url: "https://github.com/JestorNogueiro/airbnb",
     category: ["next"],
@@ -50,8 +46,9 @@ export const project: Project[] = [
   },
   {
     name: "Tesla",
-    description: "this is clone made with react js",
-    image_path: "/scene.jpg",
+    description:
+      "In this you will see only UI of the Tesla website, and some animation and also the menu list is available",
+    image_path: "/project/tesla.jpg",
     deployed_url: "https://tesla-clone-flax.vercel.app/",
     github_url: "https://github.com/JestorNogueiro/Tesla-clone",
     category: ["react"],
@@ -59,7 +56,8 @@ export const project: Project[] = [
   },
   {
     name: "Netflix",
-    description: "this is clone made with react js",
+    description:
+      "This is netflix like website inspired with its UI ,In this project the movie poster will display with different genre available like Netflix original, Horror, Action, etc. And also view the trailer. (Named as JestFlix)",
     image_path: "/scene.jpg",
     deployed_url: "https://netflix-clone-129c2.web.app/",
     github_url: "https://github.com/JestorNogueiro/Netflix-Clone",
@@ -68,8 +66,9 @@ export const project: Project[] = [
   },
   {
     name: "Covid Tracker",
-    description: "this is clone made with next js",
-    image_path: "/scene.jpg",
+    description:
+      "Its Covid19 tracker, you can select different country available from the dropdown items, also you can view the map with the total number of cases, recovered cases and total number of deaths by country ",
+    image_path: "/project/covid.jpg",
     deployed_url: "https://jestor-covid19-tracker.web.app/",
     github_url: "https://github.com/JestorNogueiro/covid19-tracker",
     category: ["react"],
@@ -83,11 +82,12 @@ export const project: Project[] = [
   },
   {
     name: "Amazon clone",
-    description: "this is clone made with next js",
-    image_path: "/scene.jpg",
+    description:
+      "its an e-commerce website, Inspired with the Amazon website   ",
+    image_path: "/project/amazon.jpg",
     deployed_url: "",
     github_url: "https://github.com/JestorNogueiro/amazon-clone",
     category: ["react"],
-    tech: ["react.js", "material UI", "firebase"],
+    tech: ["react.js", "contextAPI", "material UI", "firebase"],
   },
 ];
