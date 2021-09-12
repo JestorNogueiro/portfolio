7ff3bd9593feea5b41d49cd15f619b0c74b3e4bf
JestorNogueiro <jestornogueiro789@gmail.com>
2021-09-12 17:58:06 +0530

    adjusted social media icon

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -23,6 +23,39 @@ function Sidebar() {
       <h2 className="my-3 text-3xl font-medium tracking-wider font-Lobster">
         Jestor Nogueiro
       </h2>
+
+      {/* Social Media Details */}
+      <div className="flex items-center justify-center gap-3.5">
+        <a
+          href="https://www.facebook.com/jestor.nogueiro/"
+          target="_blank "
+          rel="noreferrer"
+        >
+          <FaFacebook className="w-8 h-8 text-blue-700 transform hover:scale-110 duration-500 drop-shadow-xl" />
+        </a>
+        <a
+          href="https://www.instagram.com/jestor_nogueiro/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaInstagram className="w-8 h-8 text-pink transform hover:scale-110 duration-500 drop-shadow-xl" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jestor-nogueiro-02467b184/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaLinkedin className="w-8 h-8 text-blue-600 transform hover:scale-110 duration-500 drop-shadow-xl" />
+        </a>
+        <a
+          href="https://github.com/JestorNogueiro"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithub className="w-8 h-8 transform hover:scale-110 duration-500 drop-shadow-xl" />
+        </a>
+      </div>
+
       <p className="bg-gray-200 dark:bg-gray-100 dark:text-black font-semibold my-3 px-8 py-1.5 rounded-full max-w-min mx-auto whitespace-nowrap">
         Front-End Developer
       </p>
@@ -44,38 +77,6 @@ function Sidebar() {
         </div>
         <p className="my-2 font-semibold ">jestornogueiro789@gmail.com</p>
         <p className="my-2">+91 9823145824 / +91 6366705572</p>
-
-        {/* Social Media Details */}
-        <div className="flex items-center justify-center gap-3.5">
-          <a
-            href="https://www.facebook.com/jestor.nogueiro/"
-            target="_blank "
-            rel="noreferrer"
-          >
-            <FaFacebook className="w-8 h-8 text-blue-700 transform hover:scale-110 duration-500 drop-shadow-xl" />
-          </a>
-          <a
-            href="https://www.instagram.com/jestor_nogueiro/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaInstagram className="w-8 h-8 text-pink transform hover:scale-110 duration-500 drop-shadow-xl" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/jestor-nogueiro-02467b184/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaLinkedin className="w-8 h-8 text-blue-600 transform hover:scale-110 duration-500 drop-shadow-xl" />
-          </a>
-          <a
-            href="https://github.com/JestorNogueiro"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaGithub className="w-8 h-8 transform hover:scale-110 duration-500 drop-shadow-xl" />
-          </a>
-        </div>
       </div>
 
       {/* contact */}
